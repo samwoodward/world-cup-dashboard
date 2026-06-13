b576accecba578524dadd13f445e58fcc9902e29
samwoodward <sam.woodward@ifminvestors.com>
2026-06-13 11:49:49 +0100

    Update streamlit_app_clean.py

--- streamlit_app_clean.py
+++ streamlit_app_clean.py
@@ -108,8 +108,8 @@ fixtures = [
     {"date": "2026-06-12", "stage": "Group", "group": "D", "home": "USA", "away": "Paraguay", "home_goals": 4, "away_goals": 1},
 
     # Sat 13 Jun
-    {"date": "2026-06-13", "stage": "Group", "group": "B", "home": "Qatar", "away": "Switzerland", "home_goals": 2, "away_goals": 1},
-    {"date": "2026-06-13", "stage": "Group", "group": "C", "home": "Brazil", "away": "Morocco", "home_goals": 3, "away_goals": 4},
+    {"date": "2026-06-13", "stage": "Group", "group": "B", "home": "Qatar", "away": "Switzerland", "home_goals": None, "away_goals": None},
+    {"date": "2026-06-13", "stage": "Group", "group": "C", "home": "Brazil", "away": "Morocco", "home_goals": None, "away_goals": None},
 
     # Sun 14 Jun
     {"date": "2026-06-14", "stage": "Group", "group": "C", "home": "Haiti", "away": "Scotland", "home_goals": None, "away_goals": None},
