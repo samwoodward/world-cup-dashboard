4e305040ba657bfbc5e9fb5b13fb397dedaef43b
samwoodward <sam.woodward@ifminvestors.com>
2026-07-05 06:10:52 +0100

    Update streamlit_app_clean.py

--- streamlit_app_clean.py
+++ streamlit_app_clean.py
@@ -417,8 +417,8 @@ fixtures = [
     # ROUND OF 16
     # -------------------------
 
-    {"date": "2026-07-04", "stage": "R16", "home": "Paraguay", "away": "France", "home_goals": 0, "away_goals": 1},
-    {"date": "2026-07-04", "stage": "R16", "home": "Canada", "away": "Morocco", "home_goals": 0, "away_goals": 3},
+    {"date": "2026-07-04", "stage": "R16", "home": "Paraguay", "away": "France", "home_goals": None, "away_goals": None},
+    {"date": "2026-07-04", "stage": "R16", "home": "Canada", "away": "Morocco", "home_goals": None, "away_goals": None},
     {"date": "2026-07-05", "stage": "R16", "home": "Brazil", "away": "Norway", "home_goals": None, "away_goals": None},
     {"date": "2026-07-05", "stage": "R16", "home": "Mexico", "away": "England", "home_goals": None, "away_goals": None},
     {"date": "2026-07-06", "stage": "R16", "home": "Portugal", "away": "Spain", "home_goals": None, "away_goals": None},
